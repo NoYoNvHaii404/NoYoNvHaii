c78d1c525256268a4b6f68adaecf407721cf95c2
NoYoNvHaii404 <161001753+NoYoNvHaii404@users.noreply.github.com>
2024-02-23 02:27:16 -0600

    Add files via upload

---
+++ x.py
@@ -0,0 +1,12 @@
+
+# Bold High Intensty
+biblack="\033[1;90m"      # Black
+bired="\033[1;91m"        # Red
+bigreen="\033[1;92m"      # Green
+biyellow="\033[1;93m"     # Yellow
+biblue="\033[1;94m"       # Blue
+bipurple="\033[1;95m"     # Purple
+bicyan="\033[1;96m"       # Cyan
+biehite="\033[1;97m"      # White
+
+print(bicyan+"\n\t♥♥"+bigreen+"\t RisHaD SoBuJ"+bicyan+"\t♥♥")
